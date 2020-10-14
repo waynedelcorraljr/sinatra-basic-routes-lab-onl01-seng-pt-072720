e85a732e96db02f6f2bf1d1498c0b90c60f451ce
Wayne del Corral Jr <wdelcorjr@gmail.com>
2020-10-13 21:47:31 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+    "My name is Wizard"
+end
+
+get '/hometown' do
+    "My hometown is New Orleans"
+end
+
+get '/favorite-song' do
+    "My favorite song is Unstable Condition"
+end
+
 end
